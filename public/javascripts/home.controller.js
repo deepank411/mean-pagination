4cbed9fce763f382c144b659d18c42fcdc15f314
Deepank Agarwal <deepank411@gmail.com>
2016-12-14 17:23:28 +0530

    code revamp

--- public/javascripts/home.controller.js
+++ public/javascripts/home.controller.js
@@ -1,21 +1,4 @@
 app.controller("homeCtrl", ['$scope', '$http', function($scope, $http){
-   console.log('test');
-
-   $scope.currentPage = 1;
-
-   $http({
-      url: '/fetch',
-      method: "GET",
-      params: {pageNum: $scope.currentPage}
-   })
-   .then(function(response){
-      console.log(response);
-      $scope.entries = response.data.docs;
-      $scope.total = response.data.total;
-      $scope.pageSize = response.data.limit;
-   }, function(response){
-      console.log(response);
-   });
 
    $scope.pagingFunction = function(page){
       $http({
@@ -32,6 +15,5 @@ app.controller("homeCtrl", ['$scope', '$http', function($scope, $http){
          console.log(response);
       });
    }
-
 
 }]);
